e701f56fff892dfae10b4f33a14e7227bd8ec140
Mason Osborn <osbornm@wit.edu>
2024-01-31 16:37:19 -0500

    Fix build script permissions

--- build_release.sh
+++ build_release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# build for linux
 deno compile --target x86_64-unknown-linux-gnu --allow-read --allow-sys -o text-adventure-c04-linux index.js
+# build for windows
 deno compile --target x86_64-pc-windows-msvc --allow-read --allow-sys -o text-adventure-c04-win index.js
